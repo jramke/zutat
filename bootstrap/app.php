f332b7c27a561b26e691374f825fc21c6fe54d23
jramke <jstrmk@gmail.com>
2025-01-27 11:57:25 +0100

    add traefik proxy header

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -21,7 +21,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
             Request::HEADER_X_FORWARDED_HOST |
             Request::HEADER_X_FORWARDED_PORT |
-            Request::HEADER_X_FORWARDED_PROTO
+            Request::HEADER_X_FORWARDED_PROTO |
+            Request::HEADER_X_FORWARDED_TRAEFIK
         );
     })
     ->withExceptions(function (Exceptions $exceptions) {
