dea9af931335cd8425a38413118dd759ec51bc8c
jramke <jstrmk@gmail.com>
2025-01-27 12:27:17 +0100

    fix svg as prefered favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="icon" href="/favicon.ico" sizes="32x32">
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
 
         <title inertia>{{ config('app.name', 'Zutat') }}</title>
