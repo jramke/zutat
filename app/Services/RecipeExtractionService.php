071b8191c051354685f015e9123bf6b582c1c058
jramke <jstrmk@gmail.com>
2025-02-17 22:18:34 +0100

    Test with newHeadless option

--- app/Services/RecipeExtractionService.php
+++ app/Services/RecipeExtractionService.php
@@ -44,6 +44,7 @@ class RecipeExtractionService
                         'disable-dev-shm-usage',
                         'disable-gpu',
                     ])
+                    ->newHeadless()
                     ->timeout(300)
                     ->userAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
                     ->setEnvironmentOptions([
